431d146dd56fb1a6fcb596678779b664f31211bd
Stephen Hanna <stephenh126_@hotmail.co.uk>
2024-02-05 12:36:19 +0000

    test removed

--- src/test/java/com/stephenhanna/trebleapi/TrebleApiApplicationTests.java
+++ src/test/java/com/stephenhanna/trebleapi/TrebleApiApplicationTests.java
@@ -3,11 +3,7 @@ package com.stephenhanna.trebleapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TrebleApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class TrebleApiApplicationTests {
+//
+//}
